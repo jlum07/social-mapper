91d2a0192c1ee7c04635cc18b70985b2597d4693
Justin Lum <j.lum07@gmail.com>
2018-06-26 02:56:32 +0000

    Setting up dev branch

--- components/locations.js
+++ components/locations.js
@@ -7,9 +7,13 @@ class LocationsScreen extends React.Component {
   static navigationOptions = { header: null };
 
   constructor(props) {
+
     super(props);
 
+
+
     this.getLocations = this.getLocations.bind(this);
+
   }
 
   getLocations (lat, lng) {
