f7d1f70aa2f6d15b3cee13b03d0d939b67417f07
Justin <j.lum07@gmail.com>
2018-10-06 19:53:03 -0400

    Reworked home screen with native base

--- components/home.js
+++ components/home.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, View, Text, Button } from 'react-native';
+import { AppRegistry, StyleSheet, View, Text } from 'react-native';
+
+import { Container, Content, Button, Body, Icon, H1, H2, H3 } from 'native-base';
+
 import { createStackNavigator } from 'react-navigation';
+
 
 class HomeScreen extends React.Component {
 
@@ -8,15 +12,30 @@ class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container} >
-        <Text style={styles.title} >Welcome to Insta Buddy!</Text>
-        <Text style={styles.description} > Tap on map to view or long press to see nearby locations.</Text>
-        <Button
-          style={ styles.homeButton }
-          title="Start Exploring"
-          onPress={() => this.props.navigation.navigate('Map')}
-        />
-      </View>
+      <Container style={styles.container} >
+        <Content contentContainerStyle={styles.content}>
+
+          <H1 style={styles.title} >Welcome to Insta Buddy!</H1>
+          <H3 style={styles.description} > Tap on map to view or long press to see nearby locations.</H3>
+
+          <Button success large
+            style={styles.homeButton}
+            onPress={() => this.props.navigation.navigate('Map')} >
+            <Text style={styles.buttonText} >
+              Start Exploring
+            </Text>
+          </Button>
+
+          <Button warning large
+            style={styles.homeButton}
+            onPress={() => this.props.navigation.navigate('MapTest')} >
+            <Text style={styles.buttonText} >
+              Beta Test
+            </Text>
+          </Button>
+
+        </Content>
+      </Container>
     );
   }
 }
@@ -29,24 +48,36 @@ export default createStackNavigator({
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#0072c4',
+  },
+  content: {
     flex: 1,
-    backgroundColor: '#42f4d9',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     fontSize: 30,
     color: '#fff',
+    textAlign: 'center',
     marginBottom: 100
   },
   description: {
+    textAlign: 'center',
     fontSize: 20,
     color: '#fff',
-    marginBottom: 100
+    marginBottom: 75
   },
   homeButton: {
-    color: 'red',
-    padding: 20,
+    alignSelf: 'center',
+    padding: 15,
+    margin: 20
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: '400',
+    width: 120,
+    textAlign: 'center',
   }
 });
 
